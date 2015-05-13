de56de8b0d56dbb2715ed98595c760d4b78d9198
Diederik Kroondijk <dkroondijk@hotmail.com>
2015-05-13 01:02:24 -0700

    styling and landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,14 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by_id session[:user_id]
   end
+  helper_method :current_user
 
   def authenticate_user!
     redirect_to new_session_path, alert: "Please Sign In" unless current_user
   end
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
 end
